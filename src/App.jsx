ef4b123e4992c8845130e1b3b76576d40888d225
0326 <winniringy@gmail.com>
2025-10-19 01:29:32 +0800

    fix: github page visit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import HistoryPage from './pages/History';
-import HomePage from './pages/Home';
-import ChartPage from './pages/Chart';
-import './App.less';
+import HistoryPage from "./pages/History";
+import HomePage from "./pages/Home";
+import ChartPage from "./pages/Chart";
+import "./App.less";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const hash = location.hash;
 
-  useEffect(() => {
-  }, [])
+  useEffect(() => {}, []);
+
+  const render = () => {
+    switch (hash) {
+      case "#chart":
+        return <ChartPage />;
+      case "#history":
+        return <HistoryPage />;
+      default:
+        return <HomePage />;
+    }
+  };
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/macau888' element={<HomePage />} />
-      <Route path='/macau888/chart' element={<ChartPage />} />
-      <Route path='/macau888/history' element={<HistoryPage />} />
-      <Route path='*' element={<HistoryPage/>} />
+      <Route path="*" element={render()} />
     </Routes>
   );
 };
